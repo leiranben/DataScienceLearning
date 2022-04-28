b9b1da2a4ff267c56171cd41b81e35343bc04a85
leira004 <leira004@umn.edu>
2022-04-28 14:20:18 -0500

    Final commit for today... just getting started.

--- LearningProject1.py
+++ LearningProject1.py
@@ -1,4 +1,6 @@
-# First project in learning data science... not really sure what will actually happen yet
+# Learning how to implement classifier models using the Wisconsin Breast Cancer Dataset
+# Dataset is linked here: https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+(Diagnostic) 
+
 
 # Imports
 import numpy as np
@@ -16,31 +18,34 @@ from sklearn.neighbors import KNeighborsClassifier
 # Loading some data from the Wisconsin Breast Cancer dataset
 data = load_breast_cancer(as_frame=True)
 df = data.frame
-features = data.feature_names
-
+# features = data.feature_names
+features = ["worst concave points", "worst radius",  "mean concave points","worst perimeter"]
 # Testing some basic statistics
 # print(df.describe())
 
 # Trying to predict targets based on the ten parameters given
-X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size = 0.2, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(data.data[["worst concave points", 
+                                                              "worst radius", 
+                                                              "mean concave points",
+                                                              "worst perimeter"]], data.target, test_size = 0.2, random_state = 42)
 
 # Logistic Regression
-print("Logsitic Regression Model")
+# print("Logsitic Regression Model")
 reg_log = LogisticRegression()
 reg_log.fit(X_train, y_train)
 y_pred_log = reg_log.predict(X_test)
-print(metrics.classification_report(y_test, y_pred_log))
-print("roc_auc_score: ", metrics.roc_auc_score(y_test, y_pred_log))
-print("f1 score: ", metrics.f1_score(y_test, y_pred_log))
+# print(metrics.classification_report(y_test, y_pred_log))
+# print("roc_auc_score: ", metrics.roc_auc_score(y_test, y_pred_log))
+# print("f1 score: ", metrics.f1_score(y_test, y_pred_log))
 
 # Random Forest Classifier Model
-print("Random Forest Classifier Model")
+# print("Random Forest Classifier Model")
 reg_rf = RandomForestClassifier()
 reg_rf.fit(X_train, y_train)
 y_pred_rf = reg_rf.predict(X_test)
-print(metrics.classification_report(y_test, y_pred_rf))
-print("roc_auc_score: ", metrics.roc_auc_score(y_test, y_pred_rf))
-print("f1 score: ", metrics.f1_score(y_test, y_pred_rf))
+# print(metrics.classification_report(y_test, y_pred_rf))
+# print("roc_auc_score: ", metrics.roc_auc_score(y_test, y_pred_rf))
+# print("f1 score: ", metrics.f1_score(y_test, y_pred_rf))
 
 # Looking at the importance of different features in the model
 feature_df = pd.DataFrame({'Importance':reg_rf.feature_importances_, 'Features': features })
@@ -48,22 +53,22 @@ feature_df = feature_df.sort_values(by=["Importance"])
 print(feature_df)
 
 # Support Vector Machine Model
-print("SVM Model")
+# print("SVM Model")
 reg_svm = SVC()
 reg_svm.fit(X_train, y_train)
 y_pred_svm = reg_svm.predict(X_test)
-print(metrics.classification_report(y_test, y_pred_svm))
-print("roc_auc_score: ", metrics.roc_auc_score(y_test, y_pred_svm))
-print("f1 score: ", metrics.f1_score(y_test, y_pred_svm))
+# print(metrics.classification_report(y_test, y_pred_svm))
+# print("roc_auc_score: ", metrics.roc_auc_score(y_test, y_pred_svm))
+# print("f1 score: ", metrics.f1_score(y_test, y_pred_svm))
 
 # K-Nearest Neighbors Model
-print("K-Nearest Neighbors Model")
+# print("K-Nearest Neighbors Model")
 reg_knn = KNeighborsClassifier()
 reg_knn.fit(X_train, y_train)
 y_pred_knn = reg_knn.predict(X_test)
-print(metrics.classification_report(y_test,y_pred_knn))
-print("roc_auc_score: ", metrics.roc_auc_score(y_test, y_pred_svm))
-print("f1 score: ", metrics.f1_score(y_test, y_pred_svm))
+# print(metrics.classification_report(y_test,y_pred_knn))
+# print("roc_auc_score: ", metrics.roc_auc_score(y_test, y_pred_svm))
+# print("f1 score: ", metrics.f1_score(y_test, y_pred_svm))
 
 # Summarizing the success of various models
 summary_df = pd.DataFrame({'Model':["Logistic Regression", "Random Forest", "SVM", "K-Nearest Neighbors"],
@@ -74,5 +79,5 @@ summary_df = pd.DataFrame({'Model':["Logistic Regression", "Random Forest", "SVM
                             'F1':[metrics.f1_score(y_test, y_pred_log),
                                     metrics.f1_score(y_test, y_pred_rf),
                                     metrics.f1_score(y_test, y_pred_svm),
-                                    metrics.f1_score(y_test, y_pred_knn)]})
+                                    metrics.f1_score(y_test, y_pred_knn)]}).sort_values(by=["ROC"])
 print(summary_df)
