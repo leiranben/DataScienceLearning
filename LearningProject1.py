e65ab4b5e765f265487b85f74ad471877feced0f
leira004 <leira004@umn.edu>
2022-04-28 11:55:20 -0500

    commit

---
+++ LearningProject1.py
@@ -0,0 +1,8 @@
+# First project in learning data science... not really sure what will actually happen yet
+
+# Imports
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+print("Hello world!")
